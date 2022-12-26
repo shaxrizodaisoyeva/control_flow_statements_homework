c91a89faa9304287ae1f977c27acb8b9fd8108da
shaxrizodaisoyeva <shaxrizodaisoyeva24@gmail.com>
2022-12-26 08:42:48 +0500

    4th is done

--- if04.py
+++ if04.py
@@ -8,4 +8,14 @@ def main(a,b,c):
     returns:
         integer: the number of positive numbers in the given numbers
     """
-    return
+    if a>0:
+        number = 1
+    if b>0:
+        number = number + 1
+    if c>0:
+        number = number + 1
+    return number
+a = 70
+b = -45
+c = -67
+print(main(a, b, c))
